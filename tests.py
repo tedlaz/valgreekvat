b68177ead9f7a8194996f5150806b11ab98b24f9
Ted Lazaros <tedlaz@gmail.com>
2017-04-15 13:33:31 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ class Tests_general(unittest.TestCase):
     def test_a1(self):
         assert len('abc') == 3
     def test_a2(self):
-        assert len('aaa') == 2
+        assert len('aa') == 2
         
  
 if __name__ == '__main__':
